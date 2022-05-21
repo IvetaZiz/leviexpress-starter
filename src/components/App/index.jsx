358caa48515809eec62439dc7c2538e654b84aea
Iveta Žižková <ziz.iveta@gmail.com>
2022-05-21 10:50:45 +0200

    first router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,10 +3,15 @@ import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 export const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservation" element={<h2>Ahoj</h2>} />
+    </Routes>
     <Footer />
-  </>
+  </BrowserRouter>
 );
